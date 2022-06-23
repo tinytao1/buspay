6c11bb3d34c40b6f8759f4c988390bd1d122c7e3
AGBeans <86927506+AGBeans@users.noreply.github.com>
2022-06-22 23:22:39 -0400

    Add files via upload

--- buspayfunsRefactor.js
+++ buspayfunsRefactor.js
@@ -15,18 +15,28 @@ let straightTime = 0;
 let straightTimePay = 0;
 let straightPlusOvertimePay = 0;
 
+let allOvertimePay = 0;
+
 let li1 = document.getElementById("li1");
 let li2 = document.getElementById("li2");
 let li3 = document.getElementById("li3");
 let li4 = document.getElementById("li4");
 let li5 = document.getElementById("li5");
 let li6 = document.getElementById("li6");
-let li7 = document.getElementById("li6");
-let li8 = document.getElementById("li6");
-let li9 = document.getElementById("li6");
+let li7 = document.getElementById("li7");
+let li8 = document.getElementById("li8");
+let li9 = document.getElementById("li9");
 
+let allOTBtn = document.getElementById("allOvertimeButton");
+let goBtn = document.getElementById("goBtn")
 
 function calculatePay() {
+    allOTBtn.style.backgroundColor = "rgb(242, 242, 242)";
+    allOTBtn.style.border = "2px solid rgb(128, 128, 128)";
+    allOTBtn.style.color = "black";
+    goBtn.style.backgroundColor = "rgb(140, 217, 175)";
+    goBtn.style.border = "solid 2px rgb(64, 191, 121)";
+
     rate = document.getElementById("rateInputEl").value;
     rate = parseFloat(rate);
     if (isNaN(rate)) rate = 0;
@@ -44,11 +54,20 @@ function calculatePay() {
         let fractionHour = minutes / 60; //alert(typeof fractionHour); 
         document.getElementById("showPayEl").innerHTML = "$" + pay.toFixed(2);
         li1.innerHTML = minutes + " minutes " + "/" + " 60 = " + fractionHour.toFixed(4) + " of an hour.";
-        li2.innerHTML =  time.toFixed(4) + " hours, multiplied by $" + rate + " = $" + pay.toFixed(4);
+        li2.innerHTML =  time.toFixed(4) + " hours, multiplied by $" + rate.toFixed(2) + " = $" + pay.toFixed(4);
         li1.style.display = "list-item";
         li2.style.display = "list-item";
+
+        li3.style.display = "none";
+        li4.style.display = "none";
+        li5.style.display = "none";
+        li6.style.display = "none";
+        li7.style.display = "none";
+        li8.style.display = "none";
+        li9.style.display = "none";
 }
     else {
+        let fractionHour = minutes / 60; //alert(typeof fractionHour); 
         overtime = time - 40;
         overtimeRate = rate * 1.5;
         straightTime = 40;
@@ -61,34 +80,75 @@ function calculatePay() {
 
         document.getElementById("showPayEl").innerHTML = "$" + straightPlusOvertimePay.toFixed(2); //good
 
-        li1.innerHTML = "<b>Straight time</b>" + "$" + straightTimePay;
+        li1.style.display = "list-item";
+        li1.innerHTML = "<b>Straight time:</b>" + " $" + straightTimePay.toFixed(2);
 
+        li2.style.display = "list-item";
+        li2.innerHTML = "<b>Overtime:</b>" + " $" + overtimePay.toFixed(2);
 
+        li3.style.display = "list-item";
+        li3.innerHTML = minutes + " minutes " + "/" + " 60 = " + fractionHour.toFixed(4) + " of an hour.";
+
+        li4.style.display = "list-item";
+        li4.innerHTML = time.toFixed(4) + " hours - 40 = " + overtime.toFixed(4) + " hours of overtime.";
+
+        li5.style.display = "list-item";
+        li5.innerHTML = 40 + " hours multiplied by $" + rate.toFixed(2) + " = $" + straightTimePay.toFixed(2);
+
+        li6.style.display = "list-item";
+        li6.innerHTML = "$" + rate.toFixed(2) + " /hour multiplied by 1.5 = <br>$" + overtimeRate.toFixed(4) + " /hour overtime rate.";
+
+        li7.style.display = "list-item";
+        li7.innerHTML = overtime.toFixed(4) + " hours multiplied by $" + overtimeRate.toFixed(4) + " = $" + overtimePay.toFixed(4);
+
+        li8.style.display = "list-item";
+        li8.innerHTML = "$" + straightTimePay.toFixed(2) + " + $" + overtimePay.toFixed(2) + " = $" + straightPlusOvertimePay.toFixed(2);
     }
 }
 
-/*
+function allOvertime() {
+    allOTBtn.style.backgroundColor = "rgb(255, 153, 153)";
+    allOTBtn.style.border = "2px solid rgb(128, 0, 0)";
+    allOTBtn.style.color = "rgb(128, 0, 0)";
+    goBtn.style.backgroundColor = "rgb(242, 242, 242)";
+    goBtn.style.border = "solid 2px rgb(128, 128, 128)";
 
+    let fractionHour = minutes / 60; //alert(typeof fractionHour);
+    rate = document.getElementById("rateInputEl").value;
+    rate = parseFloat(rate);
+    if (isNaN(rate)) rate = 0;
+    hours = document.getElementById("hoursInputEl").value;
+    hours = parseFloat(hours);
+    if (isNaN(hours)) hours = 0;
+    minutes = document.getElementById("minutesInputEl").value;
+    minutes = parseFloat(minutes);
+    if (isNaN(minutes)) minutes = 0;
+    time = hours + (minutes / 60);
+    pay = rate * time;
 
+    overtimeRate = rate * 1.5;
+    allOvertimePay = time * overtimeRate;
 
+    document.getElementById("showPayEl").innerHTML = "$" + allOvertimePay.toFixed(2);
 
-    
+    li1.innerHTML = "<b>All Overtime</b>"
+    li1.style.display = "list-item";
 
-*/
+    li2.innerHTML = minutes + " minutes " + "/" + " 60 = " + fractionHour.toFixed(4) + " of an hour.";
+    li2.style.display = "list-item";
 
+    li3.innerHTML = "$" + rate.toFixed(2) + " /hour multiplied by 1.5 = <br>$" + overtimeRate.toFixed(4) + " /hour overtime rate.";
+    li3.style.display = "list-item"
 
+    li4.style.display = "list-item";
+    li4.innerHTML = time.toFixed(4) + " hours, multiplied by $" + overtimeRate.toFixed(4) + " = $" + allOvertimePay.toFixed(4);
 
-// document.getElementById("rateEl").textContent = rate.toFixed(2);
-// document.getElementById("hoursEl").textContent = hours;
-// document.getElementById("minutesEl").textContent = minutes;
-
-
-
-
-
-
-
-
+    li5.style.display = "none";
+    li6.style.display = "none";
+    li7.style.display = "none";
+    li8.style.display = "none";
+    li9.style.display = "none";
+}
 
 
 
@@ -105,6 +165,11 @@ function theResetButtonActions() {
     li7.style.display = "none";
     li8.style.display = "none";
     li9.style.display = "none";
+    allOTBtn.style.backgroundColor = "rgb(242, 242, 242)";
+    allOTBtn.style.border = "2px solid rgb(128, 128, 128)";
+    allOTBtn.style.color = "black";
+    goBtn.style.backgroundColor = "rgb(140, 217, 175)";
+    goBtn.style.border = "solid 2px rgb(64, 191, 121)";
 }
 
 function onloadratesInsertIntoButtons() {
